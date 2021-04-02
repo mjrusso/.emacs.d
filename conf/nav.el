ea705af32e07de6005c5a06287783bebd2064af0
Michael Russo <mjrusso@gmail.com>
2021-04-02 16:41:35 -0400

    Use ace-window to move between windows.

--- conf/nav.el
+++ conf/nav.el
@@ -7,3 +7,11 @@
   :defer nil
   :bind (("C-;" . avy-goto-char-timer)
          ("C-'" . avy-goto-line)))
+
+;; ace-window: improved window selection.
+;;
+;; https://github.com/abo-abo/ace-window
+(use-package ace-window
+  :ensure t
+  :defer nil
+  :bind ("C-x o" . ace-window))
