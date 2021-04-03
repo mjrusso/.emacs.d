b02d3895031355379e877c19b7e3e5a03dff1d66
Michael Russo <mjrusso@gmail.com>
2021-04-03 10:22:54 -0400

    Use projectile-ripgrep with S-r, instead of projectile-grep.

--- conf/bindings.el
+++ conf/bindings.el
@@ -2,4 +2,4 @@
 
 ;; S-r: search entire project
 ;; C-u S-r (or M-1 S-r): allows user to specify search directory
-(global-set-key [(super r)] 'projectile-grep)
+(global-set-key [(super r)] 'projectile-ripgrep)
