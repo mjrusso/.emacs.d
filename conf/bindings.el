ff359979d7d243e76c5daa07d5c7dbd46131a94a
Michael Russo <mjrusso@gmail.com>
2021-04-03 10:24:14 -0400

    Add a few new Projectile-related bindings.

--- conf/bindings.el
+++ conf/bindings.el
@@ -3,3 +3,6 @@
 ;; S-r: search entire project
 ;; C-u S-r (or M-1 S-r): allows user to specify search directory
 (global-set-key [(super r)] 'projectile-ripgrep)
+
+(global-set-key [(super f)] 'projectile-find-file)
+(global-set-key [(super d)] 'projectile-find-dir)
