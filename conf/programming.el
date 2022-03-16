ea3dfb55fd35bf1f4e4d8e668ae878e44890e2f5
Michael Russo <mjrusso@gmail.com>
2022-03-16 08:28:56 -0400

    Switch from paredit to smartparens.

--- conf/programming.el
+++ conf/programming.el
@@ -121,16 +121,11 @@
   (add-hook 'lisp-interaction-mode-hook #'rainbow-delimiters-mode)
   (add-hook 'scheme-mode-hook #'rainbow-delimiters-mode))
 
-(use-package paredit
-  :config
-  (add-hook 'emacs-lisp-mode-hook #'paredit-mode)
-  (add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
-  (add-hook 'ielm-mode-hook #'paredit-mode)
-  (add-hook 'lisp-mode-hook #'paredit-mode)
-  (add-hook 'lisp-interaction-mode-hook #'paredit-mode)
-  (add-hook 'scheme-mode-hook #'paredit-mode))
-
-(add-hook 'python-mode-hook (lambda () (highlight-indentation-mode)))
+(use-package smartparens
+  :hook ((prog-mode . smartparens-mode)
+         (ielm-mode-hook . smartparens-mode))
+  :init
+  (require 'smartparens-config))
 
 (use-package toml-mode
   :mode "\\.toml\\'")
