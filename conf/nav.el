57addaddc96b58a79f401a2c3f68b8e770a2afe4
Michael Russo <mjrusso@gmail.com>
2022-03-16 08:26:30 -0400

    Add command/keybind for fast switching to/from last active window.

--- conf/nav.el
+++ conf/nav.el
@@ -6,6 +6,20 @@
   :bind (("s-j" . avy-goto-char-timer)
          ("C-'" . avy-goto-line)))
 
+;; Switch to the last active window. (Repeatedly calling this function cycles
+;; between the two most recently used windows.)
+;;
+;; Adapted from: https://emacs.stackexchange.com/a/7411
+(defun my/switch-to-last-active-window ()
+  (interactive)
+  (let ((window (get-mru-window t t t)))
+    (unless window (error "Cannot switch windows, because last window not found"))
+    (let ((frame (window-frame window)))
+      (select-frame-set-input-focus frame)
+      (select-window window))))
+
+(global-set-key (kbd "C-x i") #'my/switch-to-last-active-window)
+
 ;; ace-window: improved window selection.
 ;;
 ;; https://github.com/abo-abo/ace-window
