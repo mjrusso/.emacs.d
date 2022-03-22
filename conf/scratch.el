4c455f4031e1a79bf5f4d830b9116ba897abbefb
Michael Russo <mjrusso@gmail.com>
2022-03-22 09:02:18 -0400

    Clarify that persistent scratch files are actually persistent.

--- conf/scratch.el
+++ conf/scratch.el
@@ -1,12 +1,12 @@
-(defvar my/scratch-files-dir (file-truename "~/Dropbox/scratch/")
-  "Default location of scratch files")
+(defvar my/persistent-scratch-files-dir (file-truename "~/Dropbox/scratch/")
+  "Default location of persistent scratch files")
 
-(defun my/scratch-file ()
-  "Find or create a scratch file, defaulting to the location
-`my/scratch-files-dir'."
+(defun my/persistent-scratch-file ()
+  "Find or create a persistent scratch file, defaulting to the
+location `my/scratch-files-dir'."
   (interactive)
   (let* ((default-prefix (format-time-string "%Y-%m-%d_"))
-         (initial-path (format "%s" my/scratch-files-dir))
+         (initial-path (format "%s" my/persistent-scratch-files-dir))
          (path (read-file-name "Scratch file: "
                                initial-path nil nil default-prefix)))
     (find-file path)))
