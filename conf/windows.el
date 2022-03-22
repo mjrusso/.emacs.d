012c990fb0fc08f7b077f9053d4be56af11a5dfa
Michael Russo <mjrusso@gmail.com>
2022-03-22 09:30:00 -0400

    Prefer vertical splits.

--- conf/windows.el
+++ conf/windows.el
@@ -1,7 +1,7 @@
 ;; When splitting a window, prefer a horizontal stack rather than a vertical
 ;; stack (i.e., create the buffer to the right rather than below).
-; (setq split-width-threshold 80)
-; (setq split-height-threshold nil)
+(setq split-width-threshold 80)
+(setq split-height-threshold nil)
 
 ;; Avoid resizing.
 ; (customize-set-variable 'even-window-sizes nil)
