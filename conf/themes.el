afb1ec112633f8f458a19e79d4ab015c670e5a1d
Michael Russo <mjrusso@gmail.com>
2022-04-04 21:30:54 -0400

    Experiment with solaire-mode.
    (It's disabled now, though.)

--- conf/themes.el
+++ conf/themes.el
@@ -31,3 +31,39 @@
   "Run `my/after-load-theme-hook'."
   (run-hooks 'my/after-load-theme-hook))
 
+;; https://github.com/hlissner/emacs-solaire-mode
+(use-package solaire-mode
+
+  :disabled
+
+  :init
+
+  ;; From solaire-mode:
+  ;;
+  ;; > Emacs will always display one of *Minibuf-N* or *Echo Area N* (where X
+  ;; > is 0 or 1) in the minibuffer area. If these buffers don't exist OR they
+  ;; > exist and are empty, they will be transparent, showing the (incorrect)
+  ;; > background color of `default', but we want it to display
+  ;; > `solaire-default-face' instead, so we create these buffers early and
+  ;; > insert whitespace in them.
+  ;;
+  ;; Hack: ensure that this trailing whitespace is not highlighted.
+
+  (defun my/solaire-mode-minibuf-echo-area-hide-trailing-whitespace ()
+    (dolist (buf '(" *Minibuf-0*" " *Minibuf-1*"
+                   " *Echo Area 0*" " *Echo Area 1*"))
+      (with-current-buffer (get-buffer buf)
+        (setq-local show-trailing-whitespace nil))))
+
+  (defvar my/after-solaire-mode-fix-minibuffer-hook nil
+    "Hook run after `solaire-mode-fix-minibuffer'.")
+
+  (defadvice solaire-mode-fix-minibuffer (after run-after-solaire-mode-fix-minibuffer-hook activate)
+    "Run `my/after-solaire-mode-fix-minibuffer-hook'."
+    (run-hooks 'my/after-solaire-mode-fix-minibuffer-hook))
+
+  (add-hook 'my/after-solaire-mode-fix-minibuffer-hook #'my/solaire-mode-minibuf-echo-area-hide-trailing-whitespace)
+  (add-hook 'minibuffer-setup-hook #'my/solaire-mode-minibuf-echo-area-hide-trailing-whitespace)
+  (add-hook 'window-configuration-change-hook #'my/solaire-mode-minibuf-echo-area-hide-trailing-whitespace)
+
+  (solaire-global-mode +1))
