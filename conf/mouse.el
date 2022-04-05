3e59938b37c729c5e9d688316cc57a8572e21ce4
Michael Russo <mjrusso@gmail.com>
2022-04-04 21:09:25 -0400

    Disable the trackpad pinch gesture.

--- conf/mouse.el
+++ conf/mouse.el
@@ -7,6 +7,10 @@
   :init (setq mouse-sel-mode t)
   :config (xterm-mouse-mode t))
 
+;; Disable the trackpad pinch gesture (normally, this gesture calls
+;; `text-scale-pinch').
+(global-set-key [pinch] '(lambda () (interactive)))
+
 (global-set-key [mouse-4] '(lambda ()
                             (interactive)
                             (scroll-down 1)))
