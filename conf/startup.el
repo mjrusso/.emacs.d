da52ce27c982be87b0154a796d4c77c9d187081c
Michael Russo <mjrusso@gmail.com>
2022-04-10 13:34:06 -0400

    Display load time (and number of loaded packages) on launch.

---
+++ conf/startup.el
@@ -0,0 +1,17 @@
+;; Adapted from https://github.com/emacs-dashboard/emacs-dashboard
+(defun my/get-emacs-load-time-and-package-count ()
+  (let ((package-count 0) (time (emacs-init-time "%.2f")))
+    (when (bound-and-true-p package-alist)
+      (setq package-count (length package-activated-list)))
+    (when (boundp 'straight--profile-cache)
+      (setq package-count (+ (hash-table-count straight--profile-cache) package-count)))
+    (if (zerop package-count)
+        (format "Emacs loaded in %s seconds" time)
+      (format "%d packages loaded in %s seconds" package-count time))))
+
+(defun my/display-emacs-load-time-and-package-count ()
+  "Log how long Emacs took to load (and package count) to the minibuffer."
+  (interactive)
+  (message (my/get-emacs-load-time-and-package-count)))
+
+(add-hook 'emacs-startup-hook #'my/display-emacs-load-time-and-package-count)
