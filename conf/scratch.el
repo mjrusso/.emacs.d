0767af191c824c74b7964520f5b12162fd8a348a
Michael Russo <mjrusso@gmail.com>
2022-10-24 16:52:45 -0400

    Tweak default filename prefix for persistent scratch files.

--- conf/scratch.el
+++ conf/scratch.el
@@ -5,7 +5,7 @@
   "Find or create a persistent scratch file, defaulting to the
 location `my/scratch-files-dir'."
   (interactive)
-  (let* ((default-prefix (format-time-string "%Y-%m-%d_"))
+  (let* ((default-prefix (format-time-string "%Y-%m-%d - "))
          (initial-path (format "%s" my/persistent-scratch-files-dir))
          (path (read-file-name "Scratch file: "
                                initial-path nil nil default-prefix)))
