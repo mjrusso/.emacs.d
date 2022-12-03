560a0f502eec52f74eaee7a5a68fa0839c896e01
Michael Russo <mjrusso@gmail.com>
2022-12-03 11:10:55 -0500

    Prefer to never automatically split windows.

--- conf/windows.el
+++ conf/windows.el
@@ -1,6 +1,5 @@
-;; When splitting a window, prefer a horizontal stack rather than a vertical
-;; stack (i.e., create the buffer to the right rather than below).
-(setq split-width-threshold 80)
+;; Prefer to never automatically split windows.
+(setq split-width-threshold nil)
 (setq split-height-threshold nil)
 
 ;; Avoid resizing.
