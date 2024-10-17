4518ac9ae1bc09f54f76db1c60ced044cf0cddc9
Michael Russo <mjr@mjrusso.com>
2024-10-16 22:53:01 -0400

    Source additional environment variables from the shell.
    This includes environment variables required for nix-direnv to work properly
    with `M-x compile` and `M-x shell`; for additional details, see:
    https://github.com/purcell/envrc/issues/92

--- conf/path.el
+++ conf/path.el
@@ -1,8 +1,23 @@
 ;; Set $MANPATH, $PATH and exec-path from the shell, but only on OS X and Linux.
 ;; See https://github.com/purcell/exec-path-from-shell for more details.
+;;
+;; Also see https://github.com/purcell/envrc/issues/92 for considerations
+;; related to Nix, nix-direnv, and Fish.
 (use-package exec-path-from-shell
   :config
   (when (memq window-system '(mac ns x))
+    (dolist (var '("SSH_AUTH_SOCK"
+                   "SSH_AGENT_PID"
+                   "XDG_DATA_DIRS"
+                   "XDG_CONFIG_DIRS"
+                   "NIX_USER_PROFILE_DIR"
+                   "NIX_SSL_CERT_FILE"
+                   "NIX_PROFILES"
+                   "NIX_PATH"
+                   "__fish_nixos_env_preinit_sourced"
+                   "__NIX_DARWIN_SET_ENVIRONMENT_DONE"
+                   "__HM_SESS_VARS_SOURCED"))
+      (add-to-list 'exec-path-from-shell-variables var))
     (exec-path-from-shell-initialize)))
 
 ;; https://github.com/wbolster/emacs-direnv
