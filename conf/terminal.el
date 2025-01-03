dfef5a84e28f0e0049f9ad941a8adf812c01f014
Michael Russo <mjr@mjrusso.com>
2025-01-03 09:37:30 -0500

    TTY Emacs: use Kitty Keyboard Protocol.
    Companion config change (Ghostty terminal emulator):
    
    - https://github.com/mjrusso/nixos-config/commit/59a628857a0a5765d0c71a07df29615ba8947988
    
    Also see:
    
    - https://sw.kovidgoyal.net/kitty/keyboard-protocol/
    - https://github.com/benjaminor/kkp

--- conf/terminal.el
+++ conf/terminal.el
@@ -32,87 +32,22 @@
         :hook (after-init . global-clipetty-mode)
         )))
 
-(cl-defstruct my/modifier-chord id esc0 esc1 mods)
 
-(defconst my/modifier-chords
-  `(,(make-my/modifier-chord :id 'C :mods '(control)
-                             :esc0 "\e\[27;5;%d~" :esc1 "\e\[%d;5u")
-    ,(make-my/modifier-chord :id 'M :mods '(meta)
-                             :esc0 "\e\[27;3;%d~" :esc1 "\e\[%d;3u")
-    ,(make-my/modifier-chord :id 'C-M :mods '(control meta)
-                             :esc0 "\e\[27;7;%d~" :esc1 "\e\[%d;7u")
-    ,(make-my/modifier-chord :id 'C-S :mods '(control shift)
-                             :esc0 "\e\[27;6;%d~" :esc1 "\e\[%d;6u")
-    ,(make-my/modifier-chord :id 'C-M-S :mods '(control meta shift)
-                             :esc0 "\e\[27;8;%d~" :esc1 "\e\[%d;8u")
-    ,(make-my/modifier-chord :id 'M-S :mods '(meta shift)
-                             :esc0 "\e\[27;4;%d~" :esc1 "\e\[%d;4u")))
-
-(defconst my/modifier-chords-by-id
-  (mapcar #'(lambda (c) `(,(my/modifier-chord-id c) . ,c)) my/modifier-chords))
-
-(defconst my/char-modifiers-table
-  `(,@(mapcar
-       #'(lambda (c) `(,c C M C-M C-S C-M-S M-S))
-       `(13 127 ,@(number-sequence 32 126)))))
-
-(defun my/register-char-with-modifier-chord (char modifier-chord-id)
-  (let* ((modifier-chord
-          (alist-get modifier-chord-id my/modifier-chords-by-id))
-         (mods (my/modifier-chord-mods modifier-chord))
-         (esc0 (my/modifier-chord-esc0 modifier-chord))
-         (esc1 (my/modifier-chord-esc1 modifier-chord))
-         (key0 (if esc0 (format esc0 char) nil))
-         (key1 (if esc1 (format esc1 char) nil))
-         (event-vec (my/char-modifiers-event-vector char mods)))
-    (if key0 (define-key xterm-function-map key0 event-vec))
-    (if key1 (define-key xterm-function-map key1 event-vec))))
-
-;; This function is sourced from:
+;; Add support for the Kitty Keyboard protocol.
 ;;
-;;   https://gist.github.com/gnachman/b4fb1e643e7e82a546bc9f86f30360e4"
-;;
-(defun my/char-modifiers-event-vector (c modifiers)
-  "Apply modifiers to the character C. MODIFIERS must be a list of
-symbols amongst (meta control shift). Return an event vector."
-  (if (memq 'control modifiers) (setq c (if (and (<= ?a c) (<= c ?z))
-                                            (logand c ?\x1f)
-                                          (logior (lsh 1 26) c))))
-  (if (memq 'meta modifiers) (setq c (logior (lsh 1 27) c)))
-  (if (memq 'shift modifiers) (setq c (logior (lsh 1 25) c)))
-  (vector c))
+;; Protocol: https://sw.kovidgoyal.net/kitty/keyboard-protocol/
+;; kkp.el: https://github.com/benjaminor/kkp
+(use-package kkp
+  :ensure t
+  :config
+  (global-kkp-mode +1)
+  )
 
-;; This is adapted from:
-;;
-;;   https://gist.github.com/gnachman/b4fb1e643e7e82a546bc9f86f30360e4
-;;
-(defun my/handle-more-terminal-escapes ()
-  "Handle additional terminal escape sequences (CSI u), as
-documented at [1].
-
-This is required for Emacs with terminal-based input to handle
-some keys with modifiers in the same manner as windowed Emacs
-(ex. on macOS), such as 'C-1', 'C-2', 'C-,', 'C-;', 'C-.', and
-'C-m'.
-
-Terminals typically need to be configured to send these alternate
-sequences. (In iTerm, enable 'Report modifiers using CSI u'.)
-Otherwise, they use more conventional behaviour. For example, a
-key press of 'C-2'is transmitted by the terminal as 'C-@',which
-is bound to `set-mark-command', not the prefix 'C-u 2'.
-
-[1]: http://www.leonerd.org.uk/hacks/fixterms/"
-  (when (and (boundp 'xterm-extra-capabilities)
-             (boundp 'xterm-function-map))
-    (dolist (entry my/char-modifiers-table)
-      (let ((char (car entry))
-            (mods (cdr entry)))
-        (dolist (mod mods)
-          (my/register-char-with-modifier-chord char mod))))))
+(defun my/tty-setup-hook ()
+  (message "Running tty-setup hook..."))
 
 (use-package emacs
   :config
-  (setq xterm-extra-capabilities '(modifyOtherKeys))
   (setq xterm-set-window-title t)
   (when (not (window-system))
     (if (not (eq (tty-display-color-cells) 16777216))
@@ -120,7 +55,7 @@ is bound to `set-mark-command', not the prefix 'C-u 2'.
         (warn "TTY does not appear to support 24 bit colour")
       ))
   :hook
-  (tty-setup . my/handle-more-terminal-escapes)
+  (tty-setup . my/tty-setup-hook)
   )
 
 ;; (set 'xterm-standard-colors
