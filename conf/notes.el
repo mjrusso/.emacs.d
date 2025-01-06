9ca7365e5804a34a2a43707b6da5dc1f25991e5e
Michael Russo <mjr@mjrusso.com>
2025-01-06 10:37:28 -0500

    Stop customizing `org-todo-keywords'; defaults are fine for my needs.

--- conf/notes.el
+++ conf/notes.el
@@ -26,7 +26,7 @@
    org-use-fast-todo-selection 'expert ; https://emacs.stackexchange.com/a/69927
    )
 
-  (setq org-todo-keywords '((sequence "TODO(t)" "HOLD(h)" "NOTE(n)" "|" "DONE(d)" "DEAD(x)")))
+  ;;(setq org-todo-keywords '((sequence "TODO(t)" "HOLD(h)" "NOTE(n)" "|" "DONE(d)" "DEAD(x)")))
 
   (setq org-startup-with-inline-images t)
 
